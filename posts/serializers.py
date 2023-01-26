30373efcc83764a7f2841e6e4712ec2f1d35bd5a
RyandTeague <ryanteague@hotmail.co.uk>
2023-01-26 23:33:51 +0000

    add postdetail

--- posts/serializers.py
+++ posts/serializers.py
@@ -4,8 +4,8 @@ from .models import Post
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
-    profile_id = serializer.ReadOnlyField(source='owner.profile.id')
-    profile_image = serializer.ReadOnlyField(source='owner.profile.image.url')
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     
 
     def get_is_owner(self, obj):
@@ -28,7 +28,7 @@ class PostSerializer(serializers.ModelSerializer):
         return value
 
     class Meta:
-        model = Profile
+        model = Post
         fields = [
             'id', 'owner', 'profile_id', 'profile_image', 'created_at', 'updated_at',
              'title', 'content', 'image', 'is_owner', 'image_filter'
